aeb30582b731dd12a51a66d2cadc44a7ba0841f0
Alexander Shemetov <berkut_174@mail.ru>
2018-10-19 10:52:56 +0500

    tor_client not

--- php/common/tor_clients.php
+++ php/common/tor_clients.php
@@ -77,83 +77,83 @@ if ( ! empty( $cfg['clients'] ) ) {
             SELECT * FROM temp.ClientsNew"
         );
 
-        Db::query_database( "DELETE FROM Clients WHERE hs NOT IN (
-            SELECT Clients.hs FROM temp.ClientsNew LEFT JOIN Clients
-            ON temp.ClientsNew.hs  = Clients.hs AND temp.ClientsNew.cl = Clients.cl
-            WHERE Clients.hs IS NOT NULL
-        )");
+    }
 
-        $untracked_hashes = Db::query_database(
-            "SELECT Clients.hs FROM Clients
-            LEFT JOIN Topics ON Topics.hs = Clients.hs
-            WHERE Topics.id IS NULL AND Clients.dl IN (1,-1)",
-            array(), true, PDO::FETCH_COLUMN
-        );
+    Db::query_database( "DELETE FROM Clients WHERE hs NOT IN (
+        SELECT Clients.hs FROM temp.ClientsNew LEFT JOIN Clients
+        ON temp.ClientsNew.hs  = Clients.hs AND temp.ClientsNew.cl = Clients.cl
+        WHERE Clients.hs IS NOT NULL
+    )");
 
-        if ( ! empty( $untracked_hashes ) ) {
+    $untracked_hashes = Db::query_database(
+        "SELECT Clients.hs FROM Clients
+        LEFT JOIN Topics ON Topics.hs = Clients.hs
+        WHERE Topics.id IS NULL AND Clients.dl IN (1,-1)",
+        array(), true, PDO::FETCH_COLUMN
+    );
 
-            Log::append( "Найдено сторонних раздач: " . count( $untracked_hashes ) . " шт." );
+    if ( ! empty( $untracked_hashes ) ) {
 
-            // подключаемся к api
-            if ( ! isset( $api ) ) {
-                $api = new Api ( $cfg['api_url'], $cfg['api_key'] );
+        Log::append( "Найдено сторонних раздач: " . count( $untracked_hashes ) . " шт." );
+
+        // подключаемся к api
+        if ( ! isset( $api ) ) {
+            $api = new Api ( $cfg['api_url'], $cfg['api_key'] );
+        }
+
+        $untracked_ids = $api->get_topic_id( $untracked_hashes );
+        unset( $untracked_hashes );
+
+        if ( ! empty( $untracked_ids ) ) {
+
+            $untracked_data = $api->get_tor_topic_data( $untracked_ids );
+            unset( $untracked_ids );
+
+            if ( empty( $untracked_data ) ) {
+                throw new Exception( "Error: Не удалось получить данные о раздачах" );
             }
 
-            $untracked_ids = $api->get_topic_id( $untracked_hashes );
-            unset( $untracked_hashes );
-
-            if ( ! empty( $untracked_ids ) ) {
-
-                $untracked_data = $api->get_tor_topic_data( $untracked_ids );
-                unset( $untracked_ids );
-
-                if ( empty( $untracked_data ) ) {
-                    throw new Exception( "Error: Не удалось получить данные о раздачах" );
+            foreach ( $untracked_data as $topic_id => $topic_data ) {
+                if ( empty( $topic_data ) ) {
+                    continue;
                 }
-
-                foreach ( $untracked_data as $topic_id => $topic_data ) {
-                    if ( empty( $topic_data ) ) {
-                        continue;
-                    }
-                    $untracked_set[] = array(
-                        'id' => $topic_id,
-                        'ss' => $topic_data['forum_id'],
-                        'na' => $topic_data['topic_title'],
-                        'hs' => $topic_data['info_hash'],
-                        'se' => $topic_data['seeders'],
-                        'si' => $topic_data['size'],
-                        'st' => $topic_data['tor_status'],
-                        'rg' => $topic_data['reg_time']
-                    );
-                }
-                unset( $untracked_data );
-
-                // создаём временную таблицу
-                Db::query_database( "CREATE TEMP TABLE TopicsUntrackedNew AS
-                    SELECT id,ss,na,hs,se,si,st,rg FROM TopicsUntracked WHERE 0 = 1"
+                $untracked_set[] = array(
+                    'id' => $topic_id,
+                    'ss' => $topic_data['forum_id'],
+                    'na' => $topic_data['topic_title'],
+                    'hs' => $topic_data['info_hash'],
+                    'se' => $topic_data['seeders'],
+                    'si' => $topic_data['size'],
+                    'st' => $topic_data['tor_status'],
+                    'rg' => $topic_data['reg_time']
                 );
+            }
+            unset( $untracked_data );
 
-                $untracked_set = array_chunk( $untracked_set, 500 );
+            // создаём временную таблицу
+            Db::query_database( "CREATE TEMP TABLE TopicsUntrackedNew AS
+                SELECT id,ss,na,hs,se,si,st,rg FROM TopicsUntracked WHERE 0 = 1"
+            );
 
-                foreach ( $untracked_set as $untracked_set ) {
-                    $select = Db::combine_set( $untracked_set );
-                    unset( $untracked_set );
-                    Db::query_database( "INSERT INTO temp.TopicsUntrackedNew $select" );
-                    unset( $select );
-                }
+            $untracked_set = array_chunk( $untracked_set, 500 );
+
+            foreach ( $untracked_set as $untracked_set ) {
+                $select = Db::combine_set( $untracked_set );
                 unset( $untracked_set );
+                Db::query_database( "INSERT INTO temp.TopicsUntrackedNew $select" );
+                unset( $select );
+            }
+            unset( $untracked_set );
 
-                $count_untracked = Db::query_database(
-                    "SELECT COUNT() FROM temp.TopicsUntrackedNew",
-                    array(), true, PDO::FETCH_COLUMN
+            $count_untracked = Db::query_database(
+                "SELECT COUNT() FROM temp.TopicsUntrackedNew",
+                array(), true, PDO::FETCH_COLUMN
+            );
+
+            if ( $count_untracked[0] > 0 ) {
+                Db::query_database( "INSERT INTO TopicsUntracked (id,ss,na,hs,se,si,st,rg)
+                    SELECT * FROM temp.TopicsUntrackedNew"
                 );
-
-                if ( $count_untracked[0] > 0 ) {
-                    Db::query_database( "INSERT INTO TopicsUntracked (id,ss,na,hs,se,si,st,rg)
-                        SELECT * FROM temp.TopicsUntrackedNew"
-                    );
-                }
-
             }
 
         }
